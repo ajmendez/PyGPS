7d4e73c3cc79f32c31004ce67268cb892a4e9493
Alexander Mendez <bluespace@gmail.com>
2014-04-17 00:26:17 -0700

    added bound calc

--- pygps/gps.py
+++ pygps/gps.py
@@ -155,7 +155,6 @@ class GPSFile(object):
     
 
 
-s
 
 # @profile
 def test():
@@ -178,5 +177,52 @@ def test():
         print x
         break
 
+# http://wiki.openstreetmap.org/wiki/Mercator
+import math
+def y2lat(a):
+    return 180.0/math.pi*(2.0*math.atan(math.exp(a*math.pi/180.0))-math.pi/2.0)
+def lat2y(a):
+    return 180.0/math.pi*math.log(math.tan(math.pi/4.0+a*(math.pi/180.0)/2.0))
+
+def merc_x(lon):
+    r_major = 6378137.000
+    return r_major*math.radians(lon)
+ 
+def merc_y(lat):
+    if lat > 89.5:
+        lat = 89.5
+    if lat < -89.5:
+        lat = -89.5
+    r_major = 6378137.000
+    r_minor = 6356752.3142
+    
+    temp = r_minor/r_major
+    eccent = math.sqrt(1-temp**2)
+    phi = math.radians(lat)
+    sinphi = math.sin(phi)
+    con = eccent*sinphi
+    com = eccent/2
+    con = ((1.0-con)/(1.0+con))**com
+    ts = math.tan((math.pi/2-phi)/2)/con
+    y = 0-r_major*math.log(ts)
+    return y
+
+def getbounds(location):
+    '''http://download.geofabrik.de/north-america.html
+    http://boundingbox.klokantech.com/
+    '''
+    bounds = dict(
+        sandiego=[-117.4773385039,32.5719017933,-116.3670803398,33.2681644656,],
+        world=[-180,-90,180,90],
+    )
+    xmin,ymin,xmax,ymax = bounds[location]
+    mercbounds = merc_x(xmin), merc_y(ymin), merc_x(xmax), merc_y(ymax)
+    print '{0} {1[0]:0.2f} {1[1]:0.2f} {1[2]:0.2f} {1[3]:0.2f}'.format(location, mercbounds)
+    return mercbounds
+    
+
+
 if __name__ == '__main__':
-    test()
+    # test()
+    getbounds('world')
+    getbounds('sandiego')
